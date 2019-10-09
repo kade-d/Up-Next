6d7cc123f966855aaab3747221477101db970021
kade-d <44915998+kade-d@users.noreply.github.com>
2019-10-09 13:46:51 -0400

    Add javafx to project.

--- bsueducs222/src/main/java/bsu/edu/cs222/MainMenu.java
+++ bsueducs222/src/main/java/bsu/edu/cs222/MainMenu.java
@@ -1,6 +1,38 @@
 package bsu.edu.cs222;
 
-public class MainMenu {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-    //TODO main window
+import java.io.IOException;
+
+public class MainMenu extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainMenu.fxml"));
+
+        AnchorPane page = new AnchorPane();
+
+        try{
+            page = loader.load();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+        Scene scene = new Scene(page, 800, 600); //Controls size of window
+
+        stage.setTitle("Project 1: Wiki");
+
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
