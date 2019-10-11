77fe68196e8f6c2c62ce57134fbbf9e3ef5f109d
Adam Gackenheimer <apgack@gmail.com>
2019-10-11 15:44:00 -0400

    Add initial Tic Tac Toe functions

--- bsueducs222/src/main/java/bsu/edu/cs222/TicTacToe.java
+++ bsueducs222/src/main/java/bsu/edu/cs222/TicTacToe.java
@@ -3,4 +3,10 @@ package bsu.edu.cs222;
 public class TicTacToe {
 
     //TODO Author: Adam
+
+    TicTacToeGameState gameState = new TicTacToeGameState();
+
+    public void startGame(){
+
+    }
 }
