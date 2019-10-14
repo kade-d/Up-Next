923cd15eb2eddbe549b1bb3b8ed2820368146f17
kade-d <44915998+kade-d@users.noreply.github.com>
2019-10-14 13:12:33 -0400

    Add junit to project.

---
+++ bsueducs222/src/test/java/bsu/edu/cs222/AppTest.java
@@ -0,0 +1,20 @@
+package bsu.edu.cs222;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
