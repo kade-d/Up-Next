d0d09cedbe79eb54b5720f948ea3309b8a51b8d4
Chukeze <46633640+Chukeze@users.noreply.github.com>
2019-10-20 18:45:38 -0400

    finished the details, runs locally

--- bsueducs222/src/main/java/bsu/edu/cs222/HangManDetails.java
+++ bsueducs222/src/main/java/bsu/edu/cs222/HangManDetails.java
@@ -5,7 +5,11 @@ import javafx.scene.control.Button;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class HangManDetails {
+public class HangManDetails  extends HangmanFunctionalities {
+
+    public HangManDetails() throws IOException {
+    }
+
     //rules for how to play the game
     //gameplay that allows user to keep on playing
     public static void main (String[] arg) throws IOException {
@@ -15,10 +19,10 @@ public class HangManDetails {
                 + "If you guess correctly you win."
                 + "I can go Easy on you since you're new to the game"
                 + ", or if you think you got some guts to go go with that experience I can challenge me and press Experienced"
-                + ". If you think you're the best press Hard button to");
-        System.out.println("You can guess as many times as you want but if you guess wrong 5 times, You lose"
-                + "if you guess the correct word before then, you are truly better than me" + "Users Name");
-        System.out.println(" I have picked my word, can you guess it."
+                + ". If you think you're the best press Hard button to"
+                +"You can guess as many times as you want but if you guess wrong 5 times, You lose"
+                + "if you guess the correct word before then, you are truly better than me" + "Users Name"
+                +" I have picked my word, can you guess it."
                 + "Below is a picture, and below that is your current guess, which starts off as nothing."
                 + "Everytime, you guess incorrectly, I add a body part to the picture. When it is full"
                 + "you lose!");
--- bsueducs222/src/main/java/bsu/edu/cs222/Hangman.java
+++ bsueducs222/src/main/java/bsu/edu/cs222/Hangman.java
@@ -2,15 +2,10 @@ package bsu.edu.cs222;
 
 import java.io.IOException;
 
-public class Hangman extends HangmanFunctionalities {
-
-    //TODO Author:
-    HangManDetails details= new HangManDetails();
+public class Hangman extends HangManDetails {
 
     public Hangman() throws IOException {
     }
 
-    public void startGame(){
 
-    }
 }
