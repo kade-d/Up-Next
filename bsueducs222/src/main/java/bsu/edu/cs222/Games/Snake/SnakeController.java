44f8685c1c2e26ce97dd4f24c95387f20c39812d
Adam Gackenheimer <apgack@gmail.com>
2019-11-16 21:45:43 -0500

    Improve snake

--- bsueducs222/src/main/java/bsu/edu/cs222/Games/Snake/SnakeController.java
+++ bsueducs222/src/main/java/bsu/edu/cs222/Games/Snake/SnakeController.java
@@ -1,15 +1,20 @@
 package bsu.edu.cs222.Games.Snake;
-
 import bsu.edu.cs222.Controller;
-import javafx.animation.Animation;
+import bsu.edu.cs222.FileIO.Game;
 import javafx.animation.AnimationTimer;
-import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Rectangle;
 
 import java.util.ArrayList;
+import java.util.BitSet;
 
 public class SnakeController {
 
@@ -18,19 +23,71 @@ public class SnakeController {
 
     private Controller mainController;
 
+    private BitSet keyboardBitSet = new BitSet();
+
+    private ChangeListener<Boolean> listener = null;
+
+    private int direction = 1;
+
     private Snake game;
 
-    private SnakeGameState gameState = new SnakeGameState();
+    private SnakeGameState gameState;
+
+    private int gameSpeed = 50;
+
+    private int goalLength = 400;
 
     private int mode;
 
-    private boolean gameIsRunning = true;
+    private boolean gameIsRunning = false;
+
+    private enum KEY {
+        UP(0),
+        RIGHT(1),
+        DOWN(2),
+        LEFT(3);
+
+        private final int value;
+
+        KEY(final int newValue) {
+            value = newValue;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
 
     public void initialize(Controller controller, int mode) {
         this.mainController = controller;
         this.mode = mode;
+        if(listener != null) {
+            pane.focusedProperty().removeListener(listener);
+        }
+        startSnake();
+        listener = new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean oldValue, Boolean newValue) {
+                gameIsRunning = newValue;
+                if(gameIsRunning){
+                    run();
+                }
+            }
+        };
+        pane.focusedProperty().addListener(listener);
+    }
+
+    public void startSnake(){
+        gameState = new SnakeGameState(mainController);
+        resetPane();
         addCellsToGridPane();
-        run();
+        pane.setFocusTraversable(false);
+        getFocusForGame();
+        getArrowInput();
+    }
+
+    private void resetPane(){
+        pane.getChildren().clear();
     }
 
     private void addCellsToGridPane() {
@@ -47,6 +104,15 @@ public class SnakeController {
     public void run() {
         AnimationTimer snakeTimer = makeSnakeTimer();
         snakeTimer.start();
+        if(gameState.snake.size() >= goalLength){
+            endSnake();
+        }
+    }
+
+    private void endSnake(){
+        mainController.saveWinToXML(new Game("Snake", true, "0"));
+        mainController.notifyGauntletCompleted();
+        gameIsRunning = false;
     }
 
 
@@ -68,25 +134,133 @@ public class SnakeController {
             @Override
             public void handle(long now) {
                 long newTime = System.currentTimeMillis();
-                if (timestamp + 1 <= newTime) {
-                    updateSnake();
-                    run();
+                if (timestamp + gameSpeed <= newTime) {
+                    if(gameIsRunning) {
+                        updateSnake();
+                        run();
+                    }
                     super.stop();
                 }
             }
         };
     }
 
+    private void getFocusForGame() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                pane.requestFocus();
+            }
+        });
+    }
+
     private void updateSnake(){
-        gameState.moveSnake(0);
+        gameState.moveSnake();
+        mainController.gameNotificationLabel.setText("Goal: (" + gameState.snake.size() + "/" + goalLength + ")");
         render();
+    }
+
+    private void getArrowInput() {
+        pane.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (checkRightKeyCode(event)) {
+                    keyboardBitSet.set(KEY.RIGHT.getValue());
+
+                } else if (checkLeftKeyCode(event)) {
+                    keyboardBitSet.set(KEY.LEFT.getValue());
+
+                } else if (checkDownKeyCode(event)) {
+                    keyboardBitSet.set(KEY.DOWN.getValue());
+
+                } else if (checkUpKeyCode(event)) {
+                    keyboardBitSet.set(KEY.UP.getValue());
+                }
+                setDirection();
+                event.consume();
+            }
+        });
+        pane.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                final boolean noKeysInBitSet = checkNoKeysPressedInBitSet();
+                removeKeyFromBitSet(event);
+                if (checkRightKeyCode(event) && noKeysInBitSet) {
+                    keyboardBitSet.set(KEY.RIGHT.getValue(), true);
+                } else if (checkLeftKeyCode(event) && noKeysInBitSet) {
+                    keyboardBitSet.set(KEY.LEFT.getValue(), true);
+                } else if (checkDownKeyCode(event) && noKeysInBitSet) {
+                    keyboardBitSet.set(KEY.DOWN.getValue(), true);
+                } else if (checkUpKeyCode(event) && noKeysInBitSet) {
+                    keyboardBitSet.set(KEY.UP.getValue(), true);
+                }
+                setDirection();
+            }
+        });
+    }
+
+    public void setDirection(){
+        boolean rightPressed = keyboardBitSet.get(KEY.RIGHT.getValue());
+        boolean leftPressed = keyboardBitSet.get(KEY.LEFT.getValue());
+        boolean upPressed = keyboardBitSet.get(KEY.UP.getValue());
+        boolean downPressed = keyboardBitSet.get(KEY.DOWN.getValue());
+
+        if (upPressed && gameState.lastDirection != 2) {
+            gameState.direction = 0;
+        }
+        if (rightPressed  && gameState.lastDirection != 3) {
+            gameState.direction = 1;
+        }
+        if (downPressed  && gameState.lastDirection != 0) {
+            gameState.direction = 2;
+        }
+        if (leftPressed  && gameState.lastDirection != 1) {
+            gameState.direction = 3;
+        }
+    }
+
+    private boolean checkNoKeysPressedInBitSet() {
+        for (int i = 0; i < 4; i++) {
+            if (keyboardBitSet.get(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private void removeKeyFromBitSet(KeyEvent event) {
+        if (checkRightKeyCode(event)) {
+            keyboardBitSet.set(KEY.RIGHT.getValue(), false);
+        } else if (checkLeftKeyCode(event)) {
+            keyboardBitSet.set(KEY.LEFT.getValue(), false);
+        } else if (checkDownKeyCode(event)) {
+            keyboardBitSet.set(KEY.DOWN.getValue(), false);
+        } else if (checkUpKeyCode(event)) {
+            keyboardBitSet.set(KEY.UP.getValue(), false);
+        }
+    }
+    
+
+    private boolean checkRightKeyCode(KeyEvent event) {
+        return event.getCode() == KeyCode.RIGHT || event.getCode() == KeyCode.D;
+    }
+
+    private boolean checkLeftKeyCode(KeyEvent event) {
+        return event.getCode() == KeyCode.LEFT || event.getCode() == KeyCode.A;
+    }
+
+    private boolean checkUpKeyCode(KeyEvent event) {
+        return event.getCode() == KeyCode.UP || event.getCode() == KeyCode.W;
+    }
+
+    private boolean checkDownKeyCode(KeyEvent event) {
+        return event.getCode() == KeyCode.DOWN || event.getCode() == KeyCode.S;
     }
 
     private void render(){
         ArrayList<Integer> snake = gameState.snake;
-        for(int i = 1; i <= 400; i++) {
+        for(int i = 0; i < pane.getChildren().size(); i++) {
             Node cell = pane.getChildren().get(i);
-
             if(snake.contains(i)) {
                 cell.setStyle("-fx-fill: #00ff00");
             }
--- bsueducs222/src/main/java/bsu/edu/cs222/Games/Snake/SnakeGameState.java
+++ bsueducs222/src/main/java/bsu/edu/cs222/Games/Snake/SnakeGameState.java
@@ -1,5 +1,7 @@
 package bsu.edu.cs222.Games.Snake;
 
+import bsu.edu.cs222.Controller;
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -7,22 +9,33 @@ public class SnakeGameState {
 
     private int[] cells = new int[400];
     ArrayList<Integer> snake = new ArrayList<>();
+    private Controller mainController;
     public int food;
+    public int direction;
+    public int lastDirection;
 
 
 
-    SnakeGameState(){
-        snake.add(3);
-        snake.add(4);
-        snake.add(5);
+    SnakeGameState(Controller mainController){
+        this.mainController = mainController;
+        snake.add(0);
+        snake.add(1);
+        snake.add(2);
+        direction = 1;
+        lastDirection = 1;
+        placeFood();
+    }
+
+    public void reset(){
+        while(snake.size() > 3){
+            snake.remove(0);
+        }
         placeFood();
     }
 
     public void placeFood(){
-        int randomInt = (int)(Math.random() * 400 + 1);
-        System.out.println(randomInt + "");
-        System.out.println(snake.size());
-        System.out.println(snake);
+        int randomInt = (int)(Math.random() * 400);
+        System.out.println(randomInt);
         if(snake.contains(randomInt)){
             placeFood();
             return;
@@ -30,10 +43,37 @@ public class SnakeGameState {
         food = randomInt;
     }
 
-    public void moveSnake(int move){
-        move = snake.get(snake.size() - 1) + 1;
-        if(move == 401){
-            move = 1;
+    public void moveSnake(){
+        int snakeHead = snake.get(snake.size() - 1);
+        int move = snakeHead;
+        if(direction == 0) {
+            move = snakeHead - 20;
+            if(move < 0){
+                move = move + 400;
+            }
+        }
+        if(direction == 1) {
+            move = snakeHead + 1;
+            if(move % 20 == 0){
+                move = move - 20;
+            }
+        }
+        if(direction == 2) {
+            move = snakeHead + 20;
+            if(move > 399){
+                move = move - 400;
+            }
+        }
+        if(direction == 3) {
+            move = snakeHead - 1;
+            if(move % 20 == 19 || move == -1){
+                move = move + 20;
+            }
+        }
+        lastDirection = direction;
+        if(snake.contains(move) && move != snake.get(0)){
+            mainController.notifyLoss();
+            reset();
         }
         if(move == food){
             snake.add(move);
