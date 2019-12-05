5b4dcd9e58927f512569a30a3aedc83d566430d0
csgrider <csgrider@bsu.edu>
2019-12-05 12:44:09 -0500

    Get rid of warnings in SimonController.

--- bsueducs222/src/main/java/bsu/edu/cs222/Games/Simon/SimonController.java
+++ bsueducs222/src/main/java/bsu/edu/cs222/Games/Simon/SimonController.java
@@ -7,8 +7,6 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
